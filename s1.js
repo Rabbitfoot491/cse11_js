6cbc6980a6603e93d4932b5a703b29c65f84c8c9
Rabbitfoot491 <krishna.dec1205@gmail.com>
2026-02-24 10:14:58 +0530

    added file

---
+++ s1.js
@@ -0,0 +1,34 @@
+let num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="krishna shukla";
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+let sayHello=function(){
+    alert("hello world");
+}
+sayHello();
+var numberArray=[1,2,3];//static
+// var animals=new Array["dog", "cat", "horse"];//dynamic
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+
+
+
+
