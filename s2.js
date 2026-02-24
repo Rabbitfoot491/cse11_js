33b5abbdfd6c3a1bb9fd7eb0d4422404cf129275
Rabbitfoot491 <krishna.dec1205@gmail.com>
2026-02-24 10:25:46 +0530

    added

--- s2.js
+++ s2.js
@@ -3,5 +3,13 @@ var b=34;
 document.write("a+b=");
 result=a+b;
 document.write(result);
+var count;
+document.write("starting loop "+ "<br />")
+for(count=0;count <10;count++){
+    document.write("current count:"+count);
+    document.write("<br/>");
 
+    
+}
+document.write("loop stopped");
 
