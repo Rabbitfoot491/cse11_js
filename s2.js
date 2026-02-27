8251c0da44596ea9f8ea037d8556d76e694e1bd8
Rabbitfoot491 <krishna.dec1205@gmail.com>
2026-02-27 11:59:24 +0530

    added

--- s2.js
+++ s2.js
@@ -12,4 +12,19 @@ for(count=0;count <10;count++){
     
 }
 document.write("loop stopped");
+const person={
+    fanme:"ajay",
+    lname:"singh",
+    age:25
+
+
+};
+for(let x in person){
+    console.log("person details:"+x+""+person[x])
+}
+var count=0;
+while(count<10){
+    document.write("current count: "+count+"<br/>")
+    count++;
+}
 
