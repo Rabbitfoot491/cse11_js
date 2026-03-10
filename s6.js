0865e0a7dbb1479b6f00f8b2d9e0c12abaf557fb
Rabbitfoot491 <krishna.dec1205@gmail.com>
2026-03-10 09:38:09 +0530

    addded

---
+++ s6.js
@@ -0,0 +1,20 @@
+let collection=[
+    {},
+    [],
+    true,
+    "john",
+    
+    12445,
+    undefined, 
+    null,
+    new String("abc"),
+    new Date(),
+
+
+    
+    
+];
+console.log(collection);
+collection.teachername="John";
+collection.phno=9500034;
+console.log(collection);
