59251202a621671cb01724d165d2f35f27e421ee
Rabbitfoot491 <krishna.dec1205@gmail.com>
2026-03-17 10:02:18 +0530

    added

---
+++ events.js
@@ -0,0 +1,18 @@
+function onClick() {
+    console.log("click");
+}
+function onMouseOver() {
+    console.log("over");
+}
+function onMouseOut() {
+    console.log("out");
+}
+function onMouseUp() {
+    console.log("up");
+}
+function onMouseDown() {
+    console.log("down");
+} 
+function onMouseMove() {
+    console.log("move");
+}
